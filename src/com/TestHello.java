591bfe44cb42a65f5e658989abbe9a1da178385c
rahulbandgar <rahul.bandgar@gmail.com>
2017-06-11 20:59:14 +0530

    Create TestHello.java

--- src/com/TestHello.java
+++ src/com/TestHello.java
@@ -3,7 +3,7 @@ package com;
 public class TestHello {
 
 	public static void main(String[] args) {
-			System.out.println("Hello, Rahul One 21 ABE - test2 ../ashasj");
+			System.out.println("Hello, Rahul One 21 ABE - test2 abcd");
 	}
 
 }
